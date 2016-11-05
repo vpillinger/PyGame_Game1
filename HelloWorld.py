ea95b19f39f5a34d1a1429ba85eca61b8c5cc44f
vincent pill <vincent pill>
2016-11-05 17:33:53 -0400

    Playing around with the bouncing asteroid to make it more like a screen saver.

--- HelloWorld.py
+++ HelloWorld.py
@@ -2,25 +2,33 @@ import sys, pygame
 
 pygame.init()
 
-size = width, height = 320, 240
-speed = [2, 2]
+screen_size = width, height = 720, 540
+ball_speed = [1, 1]
 black = 0, 0, 0
 
-screen = pygame.display.set_mode(size)
+
+screen = pygame.display.set_mode(screen_size)
+clock = pygame.time.Clock();
 
 ball = pygame.image.load("asteroid.png").convert()
+ball = pygame.transform.scale(ball, (60,60))
 ballrect = ball.get_rect()
 
 while 1:
     for event in pygame.event.get():
         if event.type == pygame.QUIT: sys.exit()
 
-        ballrect = ballrect.move(speed)
-        if ballrect.left < 0 or ballrect.right > width:
-            speed[0] = -speed[0]
-        if ballrect.top < 0 or ballrect.bottom > height:
-            speed[1] = -speed[1]
+    ballrect = ballrect.move(ball_speed)
+    if ballrect.left < 0 or ballrect.right > width:
+        ball_speed[0] = -ball_speed[0]
+    if ballrect.top < 0 or ballrect.bottom > height:
+        ball_speed[1] = -ball_speed[1]
 
+    # Draw Screen
     screen.fill(black)
     screen.blit(ball, ballrect)
     pygame.display.flip()
+
+    # FPS measuring
+    clock.tick(60)
+    pygame.display.set_caption("fps: " + str(clock.get_fps()))
